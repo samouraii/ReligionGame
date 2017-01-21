8995f7bc38431dd978577160eb776d929d5434ae
pikachu pokemon <pikachu@pokemon.com>
2017-01-21 17:27:55 +0100

    Voici mes batiments

---
+++ ReligionGame/ReligionGame/Batiment/Batiment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace ReligionGame
+{
+    abstract class Batiment
+    {
+        public string nom
+        {
+            get
+            {
+                return nom;
+            }
+            set
+            {
+                nom = value;
+            }
+        }
+        public int coût
+        {
+            get
+            {
+                return coût;
+            }
+            set
+            {
+                coût = value;
+            }
+        }
+        public int attente
+        {
+            get
+            {
+                return attente;
+            }
+            set
+            {
+
+            }
+        }
+    
+    }
+}
